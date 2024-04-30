2d68f6761bdfe47af6b794376c28d139a9dd004c
Nitin Gouda <nitinagouda@acpce.ac.in>
2024-04-30 14:51:12 +0000

    xray, Cloud Watch, HoneyComb Setup

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta, timezone
+
 class HomeActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
+      'handle':  'Nitin Gouda',
       'message': 'Cloud is fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
@@ -24,7 +25,7 @@ class HomeActivities:
     },
     {
       'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Worf',
+      'handle':  'Sahil',
       'message': 'I am out of prune juice',
       'created_at': (now - timedelta(days=7)).isoformat(),
       'expires_at': (now + timedelta(days=9)).isoformat(),
@@ -33,7 +34,7 @@ class HomeActivities:
     },
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Garek',
+      'handle':  'Jinisha',
       'message': 'My dear doctor, I am just simple tailor',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
